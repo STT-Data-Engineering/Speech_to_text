ce35cda1aa1966e058fa2b2c8b673f6ce5eba729
Biruk Getaneh <bkgetmom@gmail.com>
2022-07-06 22:43:40 +0300

    adding article to category linking function

--- scripts/file_handler.py
+++ scripts/file_handler.py
@@ -30,3 +30,35 @@ class FileHandler():
 
         except FileNotFoundError:
             _logger.exception("File is not found")
+
+    def article_category_partition(self, df, start, end): 
+        """Split the articles and associate them with their category
+        Parameters
+        ----------
+            df: DataFrame
+                - a pandas DataFrame which represent the total dataset to be partitioned
+                
+            start: int
+                - a DataFrame starting index to start the partition 
+                
+            end: int
+                - a DataFrame ending index to partition the DataFrame upto 
+            
+        Return
+        ------
+            pandas DataFrame
+            - a pandas DataFrame which combine the article with its category
+        """
+        try:
+            combined = []
+            for i in range(start, end): 
+                txt_map = [*map(lambda s: s.strip(), re.split(pattern=r'[።"?]', string=df['article'][i]))] 
+                txt_map = filter(lambda x: len(x) > 0 and len(x.split()) > 2 and len(x.split()) < 15, txt_map)
+                combined.extend([{"category":df['category'][i], 'text': sen} for sen in txt_map])
+            
+            _logger.info("Article splitted and combined with its category successfully")
+
+        except Exception:
+            _logger.exception("Partition failed")
+                                
+        return pd.DataFrame(combined)
