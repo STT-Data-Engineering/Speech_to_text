00ddb3cd12ac71a7cc6fb3f7e0427d4fde44cfd4
Hen0k <henoktilaye7@gmail.com>
2022-07-08 01:53:46 +0300

    api: update dependencies

--- server/config.py
+++ server/config.py
@@ -12,14 +12,14 @@ def get_uuid():
 class ApplicationConfig(object):
     """ Flask application config """
     # JWT_SECRET_KEY = get_uuid()
-    JWT_SECRET_KEY = os.urandom(20).hex()
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
+    # JWT_SECRET_KEY = os.urandom(20).hex()
+    # JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
     # JWT_ACCESS_TOKEN_EXPIRES = timedelta(seconds=60)
 
-    # SECRET_KEY = os.urandom(20).hex()
+    SECRET_KEY = os.urandom(20).hex()
 
     # Flask-MongoEngine settings
-    MONGODB_ECHO = True
-    MONGODB_SETTINGS = {
-        'host': os.environ.get('MONGODB_ENDPOINT')
-    }
+    # MONGODB_ECHO = True
+    # MONGODB_SETTINGS = {
+    #     'host': os.environ.get('MONGODB_ENDPOINT')
+    # }
