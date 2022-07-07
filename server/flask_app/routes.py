6d88d2354f4a61b859270f5b32559b609a8579c2
Hen0k <henoktilaye7@gmail.com>
2022-07-08 01:55:28 +0300

    api: add kafka publisher and consumer to interact with kafka

--- server/flask_app/routes.py
+++ server/flask_app/routes.py
@@ -1,12 +1,25 @@
 """Routes for parent Flask app."""
-from flask import request, jsonify
+import sys
+sys.path.append('../scripts')
+
+from all_kafka import AllKafka
+from kafka.errors import NoBrokersAvailable
+from pprint import pprint
+from flask_app.helpers import get_uuid
 from flask_app.helpers import extract_audio
+from flask import request, jsonify
+
+
 
 def init_routes(app):
     """A factory function that takes in the server 
     object and initializes the routes.
     """
-
+    try:
+        producer = AllKafka.create_producer()
+        consumer = AllKafka.create_consumer(topic="text")
+    except NoBrokersAvailable:
+        print("NoBrokersAvailable")
 
     @app.route("/test")
     def test():
@@ -14,18 +27,41 @@ def init_routes(app):
 
     @app.route('/get_text', methods=["GET"])
     def get_text():
-        sentence = "አገራችን ከአፍሪካም ሆነ ከሌሎች የአለም አገራት ጋር ያላትን አለም አቀፋዊ ግንኙነት ወደ ላቀ ደረጃ ያሸጋገረ ሆኗል በአገር ውስጥ አራት አለም"
-        return jsonify(text=sentence)
+        print("sending text")
+        try:
+            for s in consumer:
+                print(s.value)
+            # sentence = next(consumer)
+            # print(sentence)
+                sentence = s.value
+            # sentence = "አገራችን ከአፍሪካም ሆነ ከሌሎች የአለም አገራት ጋር ያላትን አለም አቀፋዊ ግንኙነት ወደ ላቀ ደረጃ ያሸጋገረ ሆኗል በአገር ውስጥ አራት አለም"
+                return jsonify(text=sentence)
+        except NameError:
+            print("Consumer not init")
+            sentence = "አገራችን ከአፍሪካም ሆነ ከሌሎች የአለም አገራት ጋር ያላትን አለም አቀፋዊ ግንኙነት ወደ ላቀ ደረጃ ያሸጋገረ ሆኗል በአገር ውስጥ አራት አለም"
+            return jsonify(text=sentence)
 
     @app.route('/submit', methods=["POST"])
     def publish_text_audio_pair():
         audio = request.files['audio']
         sentence = audio.filename
         audio = extract_audio(audio)
-        print("sentence", sentence)
+        print(audio.shape)
+        audio = audio.tolist()
+        id = get_uuid()
+        data = {
+            "id": id,
+            "sentence": sentence,
+            "audio": audio
+        }
+        try:
+            res = producer.send("text.audio.pair", value=data)
+            print(res)
+        except NameError:
+            print("Producer not created")
+
+        # pprint(data)
 
         return "200"
-
-    
 
     return app
